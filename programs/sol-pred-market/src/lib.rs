41bd877ebf3c7afb7ce416ea58f83d412f1add4b
Kyle Pena <kyle.pena@kuzco.xyz>
2025-09-27 14:11:04 -0400

    no compiling checkpoint

--- programs/sol-pred-market/src/lib.rs
+++ programs/sol-pred-market/src/lib.rs
@@ -5,15 +5,11 @@ declare_id!("8WAGVU5JeuxzMzC3BgV2EvT1i2UPP2QSi4ABQegfck7Z");
 
 #[program]
 pub mod sol_pred_market {
+    use anchor_spl::token::{self, Transfer};
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        
-
-
-        Ok(())
-    }
-
+    
     pub fn create_market(ctx : Context<CreateMarket>, 
         market_id: String,
         fee_bps: u16,
@@ -41,36 +37,93 @@ pub mod sol_pred_market {
 
     pub fn abort_market(ctx: Context<AbortMarket>) -> Result<()> {
         let market = &mut ctx.accounts.market;
+
+        if ctx.accounts.authority.key() != market.authority {
+            return Err(ErrorCode::Unauthorized.into());
+        }
+
         market.is_resolved = true;
         market.outcome = Some(Outcome::Aborted);
 
-        // return all escrowed funds to persons who have placed bets
+        Ok(())
+    }
+
+    pub fn resolve_market(ctx : Context<ResolveMarket>, resolution : MarketResolution) -> Result<()> {
+        let market = &mut ctx.accounts.market;
+
+        if ctx.accounts.authority.key() != market.authority {
+            return Err(ErrorCode::Unauthorized.into());
+        }
+
+        market.is_resolved = true;
+        market.outcome = Some(resolution.to_outcome());
 
         Ok(())
     }
 
-    pub fn resolve_market(ctx : Context<ResolveMarket>) -> Result<()> {
+    pub fn place_bet(ctx : Context<PlaceBet>, amount : u64, wagered_outcome: MarketResolution) -> Result<()> {
         let market = &mut ctx.accounts.market;
-        market.is_resolved = true;
-        market.outcome = Some(Outcome::Yes);
+        token::transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.bettor_token_account.to_account_info(),
+                    to: ctx.accounts.escrow.to_account_info(),
+                    authority: ctx.accounts.authority.to_account_info(),
+                },
+            ),
+            amount,
+        )?;
+        
+        match wagered_outcome {
+            MarketResolution::Yes => market.yes_wagered += amount,
+            MarketResolution::No => market.no_wagered += amount,
+        }
         Ok(())
+    }
+
+    // user claims back their funds if the market is aborted
+    pub fn withdraw_after_abort(ctx : Context<WithdrawAfterAbort>) -> Result<()> {
+        let market = &mut ctx.accounts.market;
+        let bet = &mut ctx.accounts.bet;
+
+        if market.outcome != Some(Outcome::Aborted) {
+            return Err(ErrorCode::MarketIsNotAborted.into());
+        }
+
+        token::transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.escrow.to_account_info(),
+                    to: ctx.accounts.bettor_token_account.to_account_info(),
+                    authority: ctx.accounts.authority.to_account_info(),
+                },
+            ),
+            market.yes_wagered,
+        )?;
+        Ok(())
+    }
+
+    pub fn claim_reward(ctx: Context<ClaimReward>) -> Result<()> {
+        
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
 
 #[derive(Accounts)] 
 #[instruction(market_id: String)] // might better read as: instruction parameter declaration
 pub struct CreateMarket<'info> {
     #[account(
         init,
-        seeds = [ b"market", market_id.as_bytes() ],
-        bump,
         payer = authority,
-        space = 8 + Market::LEN
+        space = 8 + Market::LEN,
+        seeds = [ b"market", market_id.as_bytes() ],
+        bump
     )]
     pub market: Account<'info, Market>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,    
 
     // is an empty account used for a PDA signer
     #[account(
@@ -89,8 +142,7 @@ pub struct CreateMarket<'info> {
 
     pub mint: Account<'info, Mint>,
 
-    #[account(mut)]
-    pub authority: Signer<'info>,
+
     
     pub system_program: Program<'info, System>,
 
@@ -135,8 +187,97 @@ pub struct PlaceBet<'info> {
     )]
     pub market: Account<'info, Market>,
 
+    #[account(
+        init,
+        seeds = [b"bet", market_id.as_bytes(), authority.key().as_ref()],
+        bump,
+        payer = authority,
+        space = 8 + Bet::LEN
+    )]
+    pub bet: Account<'info, Bet>,
+
+    #[account(mut)]
+    pub escrow : Account<'info, TokenAccount>,
+
+    #[account(mut)]
+    pub bettor_token_account : Account<'info, TokenAccount>,
+
     #[account(mut)]
     pub authority: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+
+    pub token_program : Program<'info, Token>
+}
+
+#[account]
+pub struct Bet {
+    amount : u64,
+    wagered_outcome : MarketResolution,
+    bump : u8,
+}
+
+impl Bet {
+    const LEN : usize = 8 + 8 + 1 + 1;
+}
+
+
+#[derive(Accounts)]
+#[instruction(market_id : String)]
+pub struct WithdrawAfterAbort<'info> {
+    #[account(
+        seeds = [b"market", market_id.as_bytes()],
+        bump
+    )]
+    pub market: Account<'info, Market>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(
+        init,
+        seeds = [b"bet", market_id.as_bytes(), authority.key().as_ref()],
+        bump,
+        payer = authority,
+        space = 8 + Bet::LEN
+    )]
+    pub bet: Account<'info, Bet>,
+
+    #[account(mut)]
+    pub escrow : Account<'info, TokenAccount>,
+
+    #[account(mut)]
+    pub bettor_token_account : Account<'info, TokenAccount>,
+
+    pub system_program : Program<'info, System>,
+
+    pub token_program : Program<'info, Token>
+}
+
+#[derive(Accounts)]
+#[instruction(market_id : String)]
+pub struct PlaceBet<'info> {
+    #[account(
+        seeds = [b"market", market_id.as_bytes()],
+        bump
+    )]
+    pub market: Account<'info, Market>,
+
+    // the bettor
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(mut, constraint = token.owner == authority.key())]
+    pub token : Account<'info, TokenAccount>,
+
+    #[account(mut, has_one = mint)]
+    pub escrow : Account<'info, TokenAccount>,
+
+    pub mint : Account<'info, Mint>,
+
+    pub system_program: Program<'info, System>,
+
+    pub token_program: Program<'info, Token>,
 }
 
 #[account]
@@ -181,12 +322,20 @@ impl Market {
 pub enum Outcome {
     Yes,
     No,
-    Invalid,
     Aborted
 }
 
 #[derive(AnchorSerialize, AnchorDeserialize, Clone, Copy, PartialEq, Eq)]
-pub enum BetOutcome {
+pub enum MarketResolution {
     Yes,
     No,
+}
+
+impl MarketResolution {
+    fn to_outcome(&self) -> Outcome {
+        match self {
+            MarketResolution::Yes => Outcome::Yes,
+            MarketResolution::No => Outcome::No,
+        }
+    }
 }
