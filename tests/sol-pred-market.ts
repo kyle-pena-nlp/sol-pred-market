094be1a66183e34c184f7f77aadb752c59949e7f
Kyle Pena <kyle.pena@kuzco.xyz>
2025-09-27 23:38:46 -0400

    checkpoint before reorganizing codebase

--- tests/sol-pred-market.ts
+++ tests/sol-pred-market.ts
@@ -8,9 +8,14 @@ describe("sol-pred-market", () => {
 
   const program = anchor.workspace.SolPredMarket as Program<SolPredMarket>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+  const marketId = "mkt:demo";
+  const question = "Will BTC > $100k by 2027?";
+  const feeBps = 200; // 2%
+
+  it("Creates a market", async () => {
+    const [marketPda] = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("market"), Buffer.from(marketId)], program.programId);
+    const [escrowAuth] = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("escrow_authority")], program.programId);
+    const tx = await program.methods.createMarket(marketId, feeBps, question).accounts({ market: marketPda, escrowAuthority: escrowAuth }).rpc();
     console.log("Your transaction signature", tx);
   });
 });
